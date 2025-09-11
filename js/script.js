d0f1d91264ba348e33e01c83c71d99a5faec654b
MegaExplore <CHRISPEROLLA@GMAIL.COM>
2025-09-11 20:34:50 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
+const data = {
+  name: form.name.value,
+  email: form.email.value,
+  message: form.message.value,
+  "g-recaptcha-response": grecaptcha.getResponse()
+}
+
 const response = await fetch("https://megalibertacontactform.chrisperolla.workers.dev/send-email", {
-  method: "POST",                // Must be POST
+  method: "POST",
   headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({
-    name: form.name.value,
-    email: form.email.value,
-    message: form.message.value,
-    "g-recaptcha-response": grecaptcha.getResponse()
-  })
-});
+  body: JSON.stringify(data)
+})
+
+const result = await response.json()
+console.log("Worker response:", result)
 
